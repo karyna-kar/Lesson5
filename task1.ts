a3c9950fc23aecb1c8eb90f8f0e31b12326ff554
Karina <karyna.karmaza@gmail.com>
2026-01-28 19:24:02 +0200

    feat: solve task1 basic

--- task1.ts
+++ task1.ts
@@ -1,1 +1,11 @@
 // Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
+
+function sumOfNumbers(lastNumber: number = 0) {
+  let result = 0;
+  for (let startNumber = 0; startNumber <= lastNumber; startNumber++) {
+    result += startNumber;
+  }
+  return result;
+}
+
+console.log(sumOfNumbers());
