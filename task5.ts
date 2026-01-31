896379bdd2c4853f687e2c0bf49d1c20bc1b9554
Karina <karyna.karmaza@gmail.com>
2026-01-31 17:43:12 +0200

    refactoring: add getDate function

--- task5.ts
+++ task5.ts
@@ -2,19 +2,21 @@
 // Саму задачу обернуть в отдельную функцию getDate, которая принимает в качестве параметра произвольную дату в формате '2026-10-22T22:10:15'
 //* Проверить валидна ли дата в переданном параметре
 
-const now: Date = new Date();
-
 const coтvertToString = (parameter: number) => {
   return parameter.toString().padStart(2, "0");
 };
 
-const day = coтvertToString(now.getDate());
-const month = coтvertToString(now.getMonth() + 1);
-const year = coтvertToString(now.getFullYear());
-const hours = coтvertToString(now.getHours());
-const minutes = coтvertToString(now.getMinutes());
-const seconds = coтvertToString(now.getSeconds());
+const getDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const day = coтvertToString(date.getDate());
+  const month = coтvertToString(date.getMonth() + 1);
+  const year = coтvertToString(date.getFullYear());
+  const hours = coтvertToString(date.getHours());
+  const minutes = coтvertToString(date.getMinutes());
+  const seconds = coтvertToString(date.getSeconds());
 
-const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  return formattedDate;
+};
 
-console.log(formattedDate);
+console.log(getDate("2026-10-22T22:10:15"));
