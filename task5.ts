29c3a151b57b58baae8ce373ce2797f23174bd7c
Karina <karyna.karmaza@gmail.com>
2026-02-01 19:24:32 +0200

    refactoring: rename convertToString function

--- task5.ts
+++ task5.ts
@@ -2,18 +2,18 @@
 // Саму задачу обернуть в отдельную функцию getDate, которая принимает в качестве параметра произвольную дату в формате '2026-10-22T22:10:15'
 //* Проверить валидна ли дата в переданном параметре
 
-const coтvertToString = (parameter: number) => {
+const convertToString = (parameter: number) => {
   return parameter.toString().padStart(2, "0");
 };
 
 const getDate = (dateString: string) => {
   const date = new Date(dateString);
-  const day = coтvertToString(date.getDate());
-  const month = coтvertToString(date.getMonth() + 1);
-  const year = coтvertToString(date.getFullYear());
-  const hours = coтvertToString(date.getHours());
-  const minutes = coтvertToString(date.getMinutes());
-  const seconds = coтvertToString(date.getSeconds());
+  const day = convertToString(date.getDate());
+  const month = convertToString(date.getMonth() + 1);
+  const year = convertToString(date.getFullYear());
+  const hours = convertToString(date.getHours());
+  const minutes = convertToString(date.getMinutes());
+  const seconds = convertToString(date.getSeconds());
 
   const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
   return formattedDate;
