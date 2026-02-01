3756d6232e522d81011cf1c28e666efb67f3220c
Karina <karyna.karmaza@gmail.com>
2026-02-01 20:22:38 +0200

    feat: add checkFormat function

--- task5.ts
+++ task5.ts
@@ -6,17 +6,25 @@ const convertToString = (parameter: number) => {
   return parameter.toString().padStart(2, "0");
 };
 
-const getDate = (dateString: string) => {
-  const date = new Date(dateString);
-  const day = convertToString(date.getDate());
-  const month = convertToString(date.getMonth() + 1);
-  const year = convertToString(date.getFullYear());
-  const hours = convertToString(date.getHours());
-  const minutes = convertToString(date.getMinutes());
-  const seconds = convertToString(date.getSeconds());
+const checkFormat = (inputString: string) => {
+  const pattern = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/;
+  return pattern.test(inputString);
+};
 
-  const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-  return formattedDate;
+const getDate = (dateString: string) => {
+  if (checkFormat(dateString) == true) {
+    const date = new Date(dateString);
+    const day = convertToString(date.getDate());
+    const month = convertToString(date.getMonth() + 1);
+    const year = convertToString(date.getFullYear());
+    const hours = convertToString(date.getHours());
+    const minutes = convertToString(date.getMinutes());
+    const seconds = convertToString(date.getSeconds());
+
+    const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+    return formattedDate;
+  }
+  return "Used Date doesn't match specified format";
 };
 
 console.log(getDate("2026-10-22T22:10:15"));
